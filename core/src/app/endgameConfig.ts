e5326fe6bab03b7e233650d9d3495af0a0840768
Scott Burch <scott@bulldoginfo.com>
2023-04-15 21:38:00 -0600

    cleanup chain default handlers

--- core/src/app/endgameConfig.ts
+++ core/src/app/endgameConfig.ts
@@ -1,7 +1,8 @@
-import {Observable, Subject} from "rxjs";
+import {Observable, of} from "rxjs";
 import {AuthenticatedEndgame, Endgame, LogEntry} from "./endgame.js";
 import {PeerMsg} from "../p2p/peerMsg.js";
 import {getNetworkTime, EndgameGraphBundle, EndgameGraphValue, EndgameGraphMeta} from "../graph/endgameGraph.js";
+import {handler} from "../handlers/handler.js";
 
 export type ChainPair<T> = Observable<T> & {next: (v: T) => void, props: T}
 export type ChainProps<T extends keyof EndgameConfig['chains']> = EndgameConfig['chains'][T]['props']
@@ -28,23 +29,20 @@ export const newEndgameConfig = (config: Partial<EndgameConfig>) => ({
     name: config.name || `node-${getNetworkTime()}`,
     port: config.port || 11110,
     chains: {
-        log: config.chains?.log || nullChainPair<ChainProps<'log'>>(),
-        peerConnect: config.chains?.peerConnect || nullChainPair<ChainProps<'peerConnect'>>(),
-        auth: config.chains?.auth || nullChainPair<ChainProps<'auth'>>(),
-        unauth: config.chains?.unauth || nullChainPair<ChainProps<'unauth'>>(),
-        peersOut: config.chains?.peersOut || nullChainPair<ChainProps<'peersOut'>>(),
-        peerIn: config.chains?.peerIn || nullChainPair<ChainProps<'peerIn'>>(),
-        put: config.chains?.put || nullChainPair<ChainProps<'put'>>(),
-        get: config.chains?.get || nullChainPair<ChainProps<'get'>>(),
-        getMeta: config.chains?.getMeta || nullChainPair<ChainProps<'getMeta'>>()
+        log: config.chains?.log || nullHandler<'log'>(),
+        peerConnect: config.chains?.peerConnect || nullHandler<'peerConnect'>(),
+        auth: config.chains?.auth || nullHandler<'auth'>(),
+        unauth: config.chains?.unauth || nullHandler<'unauth'>(),
+        peersOut: config.chains?.peersOut || nullHandler<'peersOut'>(),
+        peerIn: config.chains?.peerIn || nullHandler<'peerIn'>(),
+        put: config.chains?.put || nullHandler<'put'>(),
+        get: config.chains?.get || nullHandler<'get'>(),
+        getMeta: config.chains?.getMeta || nullHandler<'getMeta'>()
     }
 
 } satisfies EndgameConfig as EndgameConfig)
 
-export const nullChainPair = <T>() => {
-    const subject = new Subject<T>();
-    const observable = subject.asObservable() as ChainPair<T>;
-    observable.next = (v: T) => subject.next(v);
-    return observable;
+export const nullHandler = <T extends keyof EndgameConfig['chains']>() => {
+    return handler<T>((x: ChainProps<T>) => of(x));
 };
 
