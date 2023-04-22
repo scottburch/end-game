3fc58a4b3014ac5630bfe49bca5b9106bd4ea6a6
Scott Burch <scott@bulldoginfo.com>
2023-04-21 21:58:09 -0600

    cleanup

--- core/src/test/testUtils.ts
+++ core/src/test/testUtils.ts
@@ -1,7 +1,6 @@
 import {graphOpen, GraphOpts} from "../graph/graph.js";
 import {handlers} from "../handlers/handlers.js";
 import {
-    LevelHandlerOpts,
     levelStoreGetEdgeHandler,
     levelStoreGetNodeHandler,
     levelStoreGetRelationshipsHandler,
@@ -10,36 +9,22 @@ import {
     levelStorePutNodeHandler
 } from "../handlers/store-handlers/levelStoreHandler.js";
 import {newUid} from "../utils/uid.js";
-import {map, of, switchMap} from "rxjs";
-import {rm} from "fs/promises";
-
 
 
 export const getAGraph = (opts: GraphOpts = {graphId: newUid()}) => {
-    const handlerOpts = {dir: 'xxx'};
-    return deleteDbFiles(handlerOpts).pipe(
-        switchMap(() => graphOpen({
-            ...opts,
-            handlers: {
-                putNode: handlers([levelStorePutNodeHandler(handlerOpts)]),
-                getNode: handlers([levelStoreGetNodeHandler(handlerOpts)]),
-                putEdge: handlers([levelStorePutEdgeHandler(handlerOpts)]),
-                getEdge: handlers([levelStoreGetEdgeHandler(handlerOpts)]),
-                nodesByLabel: handlers([levelStoreNodesByLabelHandler(handlerOpts)]),
-                nodesByProp: handlers([levelStoreNodesByPropHandler(handlerOpts)]),
-                getRelationships: handlers([levelStoreGetRelationshipsHandler(handlerOpts)]),
-                ...opts.handlers
-            },
-        }))
-    )
-
-    function deleteDbFiles(handlerOpts: LevelHandlerOpts) {
-        return handlerOpts.dir ?
-        of(`${process.cwd()}/${handlerOpts.dir}`).pipe(
-            switchMap(path => rm(path, {recursive: true, force: true})),
-            map(() => true)
-        ) : of(true)
-
-    }
+    const handlerOpts = {};
+    return graphOpen({
+        ...opts,
+        handlers: {
+            putNode: handlers([levelStorePutNodeHandler(handlerOpts)]),
+            getNode: handlers([levelStoreGetNodeHandler(handlerOpts)]),
+            putEdge: handlers([levelStorePutEdgeHandler(handlerOpts)]),
+            getEdge: handlers([levelStoreGetEdgeHandler(handlerOpts)]),
+            nodesByLabel: handlers([levelStoreNodesByLabelHandler(handlerOpts)]),
+            nodesByProp: handlers([levelStoreNodesByPropHandler(handlerOpts)]),
+            getRelationships: handlers([levelStoreGetRelationshipsHandler(handlerOpts)]),
+            ...opts.handlers
+        },
+    });
 };
 
