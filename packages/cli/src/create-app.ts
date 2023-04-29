17c0dbdc4e75b1dd605dcc872bc0b378ab9a038b
Scott Burch <scott@bulldoginfo.com>
2023-04-28 21:22:45 -0600

    add a yarn command to the cli

--- packages/cli/src/create-app.ts
+++ packages/cli/src/create-app.ts
@@ -1,20 +1,20 @@
 import {$, cd, fs} from 'zx'
-import {firstValueFrom, from, map, of, switchMap, tap} from "rxjs";
+import {from, map, switchMap, tap} from "rxjs";
 import {mkdir} from "fs/promises";
 import type {ExtractOptions} from "tar";
 import * as tar from "tar";
 import {absPath} from "@end-game/utils";
 
 
-
 export const createAppCmd = (dest: string) => {
     untarTemplate(dest).pipe(
+        tap(() => cd(dest)),
+        switchMap(() => $`yarn`),
         tap(() => console.log('\n\n')),
         tap(() => console.log(`You have successfully created a template for ${dest}`)),
         tap(() => console.log('Next steps\n----------')),
         tap(() => console.log(`type "cd ${dest}"`)),
-        tap(() => console.log(`type "yarn start" to start the development environment`)),
-//        tap(() => console.log('[optional] type "yarn testnet" to start a two node testnet to use for development')),
+        tap(() => console.log(`type "yarn dev" to start the development environment`)),
     ).subscribe()
 };
 
@@ -33,17 +33,3 @@ const untarTemplate = (dest: string) =>
         })
     );
 
-function doIt(dest: string) {
-    return firstValueFrom(of(dest).pipe(
-        switchMap(dest => $`git clone https://github.com/scottburch/dds-typescript-template.git ${dest}`),
-        tap(() => cd(dest)),
-        switchMap(() => fs.rm('.git', {force: true, recursive: true})),
-        switchMap(() => $`yarn`),
-        tap(() => console.log('\n\n')),
-        tap(() => console.log(`You have successfully created a template for ${dest}`)),
-        tap(() => console.log('Next steps\n----------')),
-        tap(() => console.log(`type "cd ${dest}"`)),
-        tap(() => console.log(`type "yarn start" to start the development environment`)),
-        tap(() => console.log('[optional] type "yarn testnet" to start a two node testnet to use for development')),
-    ))
-}
