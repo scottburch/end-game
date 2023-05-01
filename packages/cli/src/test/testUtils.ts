00c3b1c7904d70839a566fb071734b0f3c9e41b0
Scott Burch <scott@bulldoginfo.com>
2023-04-30 21:59:51 -0600

    changed cli test directory to packages/tmp

--- packages/cli/src/test/testUtils.ts
+++ packages/cli/src/test/testUtils.ts
@@ -1,13 +1,17 @@
 import {$, cd} from "zx";
 import {of, switchMap, tap} from "rxjs";
+import {absPath} from "@end-game/utils/absPath";
+
+const tmpDir = absPath(import.meta.url, '../../../tmp');
+
 
 export const createApp = () =>
     of(true).pipe(
         switchMap(() => $`npm uninstall -g @end-game/cli`),
         switchMap(() => $`npm install -g .`),
-        switchMap(() => $`rm -rf /tmp/endgame-test`),
-        switchMap(() => $`mkdir /tmp/endgame-test`),
-        tap(() => cd('/tmp/endgame-test')),
+        switchMap(() => $`rm -rf ${tmpDir}`),
+        switchMap(() => $`mkdir ${tmpDir}`),
+        tap(() => cd(tmpDir)),
         switchMap(() => $`endgame create-app my-app`),
-        tap(() => cd('/tmp/endgame-test/my-app'))
+        tap(() => cd(tmpDir + '/my-app'))
     );
