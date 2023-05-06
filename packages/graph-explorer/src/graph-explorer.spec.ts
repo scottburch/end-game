e01467c4a1f8657c6c1b945c83918cea03f2816f
Scott Burch <scott@bulldoginfo.com>
2023-05-06 00:53:47 -0600

    first graph explorer test passing

--- packages/graph-explorer/src/graph-explorer.spec.ts
+++ packages/graph-explorer/src/graph-explorer.spec.ts
@@ -1,14 +1,20 @@
-import {delay, firstValueFrom, switchMap} from "rxjs";
+import {delay, firstValueFrom, map, switchMap, tap} from "rxjs";
 import {compileBrowserTestCode} from "@end-game/utils/testCodeCompiler";
 import {absPath} from "@end-game/utils/absPath";
 import {openBrowser} from "@end-game/utils/openBrowser";
+import {expect} from "chai";
 
 describe('graph-explorer', () => {
-    it.skip('should open a graph explorer', () =>
+    it('should open a graph explorer', () =>
         firstValueFrom(compileBrowserTestCode(absPath(import.meta.url, 'graph-explorer-test.tsx')).pipe(
             switchMap(() => openBrowser()),
-            switchMap(page => page.click('button:text("Graph Explorer")')),
-            delay(100000000)
+            switchMap(page => page.click('button:text("Graph Explorer")').then(() => page)),
+            map(page => page.context().pages()),
+            switchMap(pages => pages[1].fill('input', 'person').then(() => pages)),
+            switchMap(pages => pages[1].click('button:text("By Node Label")').then(() => pages)),
+            map(pages => pages[1].getByText('person')),
+            switchMap(locator => locator.count()),
+            tap(count => expect(count).to.equal(3))
         ))
     )
 });
