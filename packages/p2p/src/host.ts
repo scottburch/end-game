8a8ad66278c83c89b760c3085fbd5707b64004db
Scott Burch <scott@bulldoginfo.com>
2023-06-19 22:12:39 -0600

    added logging to host

--- packages/p2p/src/host.ts
+++ packages/p2p/src/host.ts
@@ -1,11 +1,21 @@
 import {GraphWithP2p, PeerId} from "./p2pHandlers.js";
+import {Subject} from "rxjs";
+
+export type LogEntry<T = any> = {
+    code: string,
+    data: T
+}
 
 export type Host = {
     listeningPort: number,
     graphs: GraphWithP2p[],
-    hostId: PeerId
+    hostId: PeerId,
+    log: Subject<LogEntry>
 }
 
 export type HostProps = Host
 
-export const newHost = (props: HostProps) => props as Host;
+export const newHost = (props: Omit<HostProps, 'log'>) => ({
+    ...props,
+    log: new Subject()
+} as Host)
