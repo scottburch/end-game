1f8d15a60480764c1cc6070401bae1a75c10faef
Scott Burch <scott@bulldoginfo.com>
2023-06-26 02:54:22 -0600

    added test for nodesByLabel() paging

--- packages/graph/src/paging.spec.ts
+++ packages/graph/src/paging.spec.ts
@@ -1,6 +1,6 @@
 import {delay, first, firstValueFrom, last, mergeMap, of, range, skipWhile, switchMap, tap} from "rxjs";
 import {addThingNode, getAGraph} from "@end-game/test-utils";
-import {nodesByLabel} from "./graph.js";
+import {nodesByLabel, nodesByProp} from "./graph.js";
 import {expect} from "chai";
 
 describe('paging', () => {
@@ -22,6 +22,29 @@ describe('paging', () => {
                     expect(nodes[0].nodeId).to.equal('thing0006');
                     expect(nodes[4].nodeId).to.equal('thing0010');
                 })
+            ))
+        ))
+    );
+
+    it('should allow paging for nodesByProp()', () =>
+        firstValueFrom(getAGraph().pipe(
+            switchMap(graph => of(graph).pipe(
+                switchMap(graph => range(1, 15).pipe(
+                    mergeMap(n => addThingNode(graph, n)),
+                    last()
+                )),
+                switchMap(() => nodesByProp(graph, 'thing', 'name', '*', {limit: 5})),
+                tap(({nodes}) => {
+                    expect(nodes[0].nodeId).to.equal('thing0001');
+                    expect(nodes[4].nodeId).to.equal('thing0005');
+                }),
+                delay(1),
+                switchMap(() => nodesByProp(graph, 'thing', 'name', '', {limit: 5, gt: 'thing0005'})),
+                tap(({nodes}) => {
+                    expect(nodes[0].nodeId).to.equal('thing0006');
+                    expect(nodes[4].nodeId).to.equal('thing0010');
+                })
+
             ))
         ))
     )
