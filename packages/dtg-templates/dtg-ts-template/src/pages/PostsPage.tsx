90a62ffc4e09a14e69972f08304db936af846cff
Scott Burch <scott@bulldoginfo.com>
2023-06-26 22:35:25 -0600

    added load more to main posts page

--- packages/dtg-templates/dtg-ts-template/src/pages/PostsPage.tsx
+++ packages/dtg-templates/dtg-ts-template/src/pages/PostsPage.tsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {PostList} from "../components/PostList.jsx";
-import {useGraphNodesByLabel} from "@end-game/react-graph";
+import {useGraph} from "@end-game/react-graph";
 import {Post} from "../types/Post.js";
+import {debounceTime, Subscription, tap} from "rxjs";
+import {GraphNode, NodeId, nodesByLabel} from "@end-game/graph";
+import {Button} from "antd";
 
 export const PostsPage: React.FC = () => {
-    const posts = useGraphNodesByLabel<Post>('post', {reverse: true});
+    const graph = useGraph();
+    const postsSub = useRef<Subscription>();
+    const [posts, setPosts] = useState<GraphNode<Post>[]>([]);
+
+    const loadPosts = (last: NodeId) => {
+        postsSub.current && postsSub.current.unsubscribe();
+        postsSub.current = nodesByLabel<Post>(graph, 'post', {reverse: true, limit: 5, lt: last}).pipe(
+            debounceTime(50),
+            tap(({nodes}) => setPosts(nodes)),
+        ).subscribe()
+    }
+
+    useEffect(() => {
+        loadPosts('');
+        return () => postsSub.current?.unsubscribe();
+    }, [])
 
     return (
-        <PostList posts={posts}/>
+        <>
+            <PostList posts={posts}/>
+            {posts.length && <Button onClick={() => loadPosts(posts[posts.length - 1].nodeId)}>Load More</Button>}
+        </>
     )
 }
