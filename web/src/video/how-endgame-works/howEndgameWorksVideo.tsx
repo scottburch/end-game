81208c95d74da7bb18c6b555c2649a1f12c77af4
Scott Burch <scott@bulldoginfo.com>
2023-09-09 06:28:54 -0700

    video cleanup

--- web/src/video/how-endgame-works/howEndgameWorksVideo.tsx
+++ web/src/video/how-endgame-works/howEndgameWorksVideo.tsx
@@ -1,28 +1,19 @@
 import React from 'react'
-import {delay, last, map, of, repeat, switchMap, timer} from "rxjs";
+import {delay, of, switchMap, timer} from "rxjs";
 import {svg} from "./howEndgameWorksSvg.js";
-import {playFile, playSvg} from "../player/play.js";
+import {playSvg} from "../player/play.js";
 import {videoPart, SvgVideoPlayer} from "../player/SvgVideoPlayer.jsx";
 import {svgJS} from './howEndgameWorksJS.js'
-import {getSpeaker, speakerLoad, speakerPlay} from "../player/speak.js";
 
 export const HowEndgameWorksVideo: React.FC = () => (
     <SvgVideoPlayer
         sections={[
             {label: 'how data is retrieved', part: howDataIsRetrievedPart},
-            // {label: 'social', part: socialNetworkPart},
-            // {label: 'endgame', part: endgamePart}
         ]}
         svg={svg()}
         svgJS={svgJS}
     />
 );
-
-/*
-    Endgame is a peer-to-peer network for data sharing.  When a node requires some piece of data, it will ask
-    the network for other nodes that have the data.
- */
-
 
 const howDataIsRetrievedPart = () => of(undefined).pipe(
     // Endgame is a peer-to-peer network for data sharing.
@@ -60,45 +51,6 @@ const howDataIsRetrievedPart = () => of(undefined).pipe(
         switchMap(() => playSvg('', 'end')),
         delay(10000)
     ))),
-
-
-
 );
-
-const serverToServerPart = videoPart('/audio/endgame-intro/in_the_beginning.mp3',
-    playSvg('serverToServerStart', 'serverToServerData').pipe(
-        switchMap(() => playSvg('serverToServerData', 'serverToServerDataEnd')),
-        delay(1000),
-        switchMap(() => playSvg('', 'iDo1')),
-        delay(1000),
-        last()
-    )
-);
-
-const serverToPersonPart = videoPart('/audio/endgame-intro/web.mp3',
-    playSvg('serverToComputerStart', 'serverToComputerData').pipe(
-        switchMap(() => playSvg('serverToComputerData', 'serverToComputerDataEnd').pipe(
-            repeat()
-        )),
-        last()
-    )
-);
-
-const socialNetworkPart = videoPart('/audio/endgame-intro/social-media.mp3',
-    playSvg('socialNetworkStart', 'socialNetworkDataStart').pipe(
-        switchMap(() => playSvg('socialNetworkDataStart', 'socialNetworkDataEnd').pipe(
-            repeat()
-        )),
-        last()
-    )
-);
-
-const endgamePart = videoPart('/audio/endgame-intro/endgame.mp3',
-    playSvg('endgame', 'endgameEnd').pipe(
-        repeat(),
-        last()
-    ));
-
-
 
 
