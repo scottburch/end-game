db1f35656f44725a851bc3ced0e7355633342c53
Scott Burch <scott@bulldoginfo.com>
2023-10-05 18:24:16 -0700

    added github link to website

--- web/src/components/header/Header.tsx
+++ web/src/components/header/Header.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import {Logo} from "./Logo.jsx";
 import {HeaderMenu} from "./HeaderMenu.jsx";
+import {SocialLinks} from "./SocialLinks.jsx";
 
 export const Header: React.FC = () => {
     return (
         <header style={{backgroundColor: '#ab97da', display: 'flex', color: 'white'}}>
-                <Logo/>
-            <div style={{display: 'flex', flex: 1, alignItems: 'end', justifyContent: 'end', padding: 5, width: 500}}>
-                <HeaderMenu/>
+                <div>
+                    <Logo/>
+                </div>
+            <div style={{display: 'flex', flex: 1, flexDirection: 'column'}}>
+                <div style={{flex: 1, textAlign: 'end', padding: 5}}>
+                    <SocialLinks/>
+                </div>
+
+                <div style={{
+                    display: 'flex',
+                    flex: 1,
+                    alignItems: 'end',
+                    justifyContent: 'end',
+                    padding: 5,
+                }}>
+                    <HeaderMenu/>
+                </div>
             </div>
         </header>
     );
---
+++ web/src/components/header/SocialLinks.tsx
@@ -0,0 +1,8 @@
+import {SocialIcon} from "react-social-icons";
+import React from 'react'
+
+export const SocialLinks: React.FC = () => (
+    <>
+        <SocialIcon url="https://github.com/scottburch/end-game" bgColor="#ab97da" target="_blank"/>
+    </>
+)
