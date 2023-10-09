f8dcffca4a5a07a015aa556d868b2128d07f41a8
Scott Burch <scott@bulldoginfo.com>
2023-10-09 06:57:31 -0700

    fix local-state handler

--- packages/local-state/src/local-state-handlers.spec.ts
+++ packages/local-state/src/local-state-handlers.spec.ts
@@ -1,25 +1,26 @@
-import {firstValueFrom, switchMap} from "rxjs";
-import {asGraphId, graphOpen, newNode, putNode} from "@end-game/graph";
+import {firstValueFrom, switchMap, tap} from "rxjs";
+import {asGraphId, getNode, graphOpen, newNode, putNode} from "@end-game/graph";
 import {authHandlers} from "@end-game/pwd-auth";
 import {levelStoreHandlers} from "@end-game/level-store";
 import {p2pHandlers} from "@end-game/p2p";
 import {localStateHandlers} from "./local-state-handlers.js";
+import {expect} from 'chai'
 
 
 describe('local-state', () => {
     it('should not require auth to write to graph', () =>
         firstValueFrom(graphWithAuth().pipe(
             switchMap(graph => putNode(graph, newNode('', 'local.mine', {mine: true}))),
-
+            switchMap(({graph, nodeId}) => getNode(graph, nodeId, {})),
+            tap(({node}) => expect(node.props.mine).to.be.true)
         ))
     );
 });
 
 
 const graphWithAuth = () => graphOpen({graphId: asGraphId('my-graph')}).pipe(
+    switchMap(graph => localStateHandlers(graph)),
     switchMap(graph => levelStoreHandlers(graph)),
     switchMap(graph => authHandlers(graph)),
     switchMap(graph => p2pHandlers(graph)),
-    switchMap(graph => localStateHandlers(graph))
-
 );
