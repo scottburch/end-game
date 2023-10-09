0dc7ac56e1e2ab0e58f9207612c6e5685d474188
Scott Burch <scott@bulldoginfo.com>
2023-10-08 20:39:10 -0700

    local-state chain handler working

--- packages/local-state/src/local-state-handlers.ts
+++ packages/local-state/src/local-state-handlers.ts
@@ -1,15 +1,14 @@
-import type {Graph, GraphHandler} from "@end-game/graph";
+import type {Graph} from "@end-game/graph";
 import {of, tap} from "rxjs";
-import {insertHandlerBefore} from "@end-game/rxjs-chain";
+import {addChainFilter} from "@end-game/rxjs-chain";
+import type {RxjsChain} from "@end-game/rxjs-chain"
 
-
+// Don't allow auth or p2p modules unless the node label does not start with 'label.'
 export const localStateHandlers = (graph: Graph) => of(graph).pipe(
-    tap(graph => insertHandlerBefore(graph.chains.putNode, 'auth', 'local-state', authAntePutNodeHandler)),
+    tap(graph => Object.values<RxjsChain<any>>(graph.chains).forEach(chain =>
+        addChainFilter(chain, (chain, name, val) =>
+            name === 'auth' || name === 'p2p' ? of(!/local\./.test(val.node.label)) : of(true)
+        )
+    ))
 );
 
-const authAntePutNodeHandler: GraphHandler<'putNode'> = ({graph, node}) => {
-    return /^local./.test(node.label) ? (
-        of({graph, node})
-    ): of({graph, node})
-
-}
--- packages/rxjs-chain/src/index.ts
+++ packages/rxjs-chain/src/index.ts
@@ -1,2 +1,2 @@
 export type {RxjsChainFn, RxjsChain} from './rxjs-chain.js'
-export {newRxjsChain, chainNext, appendHandler, insertHandlerBefore, insertHandlerAfter} from './rxjs-chain.js'
+export {newRxjsChain, chainNext, appendHandler, insertHandlerBefore, insertHandlerAfter, addChainFilter} from './rxjs-chain.js'
