bebad8be2d5298b99723bf270acbd1e5d626a1be
Scott Burch <scott@bulldoginfo.com>
2023-10-09 12:03:35 -0700

    added test for local state

--- packages/local-state/src/local-state-handlers.spec.ts
+++ packages/local-state/src/local-state-handlers.spec.ts
@@ -1,4 +1,4 @@
-import {firstValueFrom, switchMap, tap} from "rxjs";
+import {catchError, firstValueFrom, of, switchMap, tap} from "rxjs";
 import {asGraphId, getNode, graphOpen, newNode, putNode} from "@end-game/graph";
 import {authHandlers} from "@end-game/pwd-auth";
 import {levelStoreHandlers} from "@end-game/level-store";
@@ -13,6 +13,14 @@ describe('local-state', () => {
             switchMap(graph => putNode(graph, newNode('', 'local.mine', {mine: true}))),
             switchMap(({graph, nodeId}) => getNode(graph, nodeId, {})),
             tap(({node}) => expect(node.props.mine).to.be.true)
+        ))
+    );
+
+    it('should require auth if the local prefix is not included', () =>
+        firstValueFrom(graphWithAuth().pipe(
+            switchMap(graph => putNode(graph, newNode('', 'mine', {}))),
+            catchError(err => of(err.code)),
+            tap(code => expect(code).to.equal('NOT_LOGGED_IN'))
         ))
     );
 });
