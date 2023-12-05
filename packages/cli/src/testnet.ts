e846dd43c3713337951aae2ebd3d7d2d78059184
Scott Burch <scott@bulldoginfo.com>
2023-12-05 15:26:50 -0700

    fix bug in testnet logging

--- packages/cli/src/testnet.ts
+++ packages/cli/src/testnet.ts
@@ -1,5 +1,5 @@
 import {startTestNet} from "@end-game/test-utils";
-import {delay, filter, map, merge, of, range, switchMap, tap, toArray} from "rxjs";
+import {delay, filter, from, map, mergeMap, of, range, switchMap, tap, toArray} from "rxjs";
 import {LogLevel} from "@end-game/graph";
 import {Host} from "@end-game/p2p";
 
@@ -19,15 +19,14 @@ export const testnet = ({log, graphs, dir, count}: TestnetOpts) =>
         switchMap(nodesDef => startTestNet(nodesDef, {graphId: graphs?.split(',')[0], dir})),
         tap(() => console.log('testnet started...', `[graphs: ${graphs}, dir: ${dir}]`)),
         switchMap(hosts => !!log ? logger(log, Object.values(hosts)) : of(undefined)),
-        map(() => {}),
+        map(() => {
+        }),
         delay(Math.pow(2, 24))
     );
 
-        const logger = (level: string, hosts: Host[]) =>
-        merge(
-            hosts[0].graphs[0].chains.log,
-            hosts[1].graphs[0].chains.log
-        ).pipe(
-            filter(({item}) => item.level <= logLevels.indexOf(level.toUpperCase())),
-            tap(({item}) => console.log(JSON.stringify(item))),
-        );
+const logger = (level: string, hosts: Host[]) =>
+    from(hosts).pipe(
+        mergeMap(host => host.graphs[0].chains.log),
+        filter(({item}) => item.level <= logLevels.indexOf(level.toUpperCase())),
+        tap(({item}) => console.log(JSON.stringify(item))),
+    );
